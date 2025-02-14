11d3df95a651f3a32dab3cd8b0d9d289fd5025ba
AbolfazlMohammady <abolfazlking22@gmail.com>
2025-02-14 00:50:49 -0800

    serializer

---
+++ blog/serializer.py
@@ -0,0 +1,76 @@
+from rest_framework import serializers
+
+from core.serializer import UserSerializer
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source ='author.full_name',read_only= True )
+    category = serializers.CharField(source ='get_category_display')
+    status = serializers.CharField(source ='get_status_display',read_only= True)
+
+    class Meta:
+        model = Post
+        fields = ['title','description','category','image','status','author','created_at','update_at']
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+
+        if request and request.method == "PATCH" and 'image' not in validated_data:
+            validated_data['image'] = instance.image  
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+class CustomPostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only= True)
+    category = serializers.CharField(source ='get_category_display')
+    status = serializers.CharField(source ='get_status_display',read_only= True)
+
+    class Meta:
+        model = Post
+        fields = ['id','title','description','category', 'status','image','author','created_at','update_at']
+ 
+
+class CreatePostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source ='author.full_name',read_only= True )
+    class Meta:
+        model = Post
+        fields = ['title','description','category','image','status','author','created_at','update_at']
+        read_only_fields = ['status']
+
+    def create(self, validated_data):
+        author = self.context['request'].user
+        validated_data['author'] = author
+        validated_data['status'] = 'UR'
+
+        return super().create(validated_data)
+    
+
+class UpdatePostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source ='author.full_name',read_only= True )
+    class Meta:
+        model = Post
+        fields = ['title','description','category','image','status','author','created_at','update_at']
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+
+        if request and request.method == "PATCH" and 'image' not in validated_data:
+            validated_data['image'] = instance.image  
+            
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+    
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id','content', 'post','user', 'created_at']

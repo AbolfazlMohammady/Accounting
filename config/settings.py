a8b0c2ad628380e2c40abadd0e8efb3206938678
AbolfazlMohammady <abolfazlking22@gmail.com>
2025-02-14 00:51:13 -0800

    all commit

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     #My App
     'core',
     'support',
+    'blog',
 
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path , include
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -8,7 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls')),
     path('api/v1/', include('support.urls')),
+    path('api/v1/', include('blog.urls')),
     path('health/', include('health_check.urls')),
 ]
 if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
